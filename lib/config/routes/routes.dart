21267606e8b555f5a3a9dd62f91fe41e86108b93
firthunsps@gmail.com <firthunsps@gmail.com>
2022-01-10 19:54:33 +0100

    la base del proyecto diciembre 2021, ultimo dia de clase

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,8 @@
+import 'package:delibery_app/pages/login/login_page.dart';
+import 'package:delibery_app/pages/welcome_page/welcome_page.dart';
+import 'package:flutter/widgets.dart';
+
+final routes = <String, WidgetBuilder>{
+  'welcome':(BuildContext context)=> WelcomePage(),
+  'login': (BuildContext context) => LoginPage()
+};
---
+++ lib/pages/welcome_page/welcome_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('images/fondo-welcome.jpeg')
+              )
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
